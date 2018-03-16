d1a03acc0be84c9aa3a68ee119907755533c261c
Toshiki Takezawa <tsk.take815@gmail.com>
2018-03-17 02:00:25 +0900

    Initial Commit

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import ReaderTests
+
+XCTMain([
+    testCase(ReaderTests.allTests),
+])
---
+++ Tests/ReaderTests/ReaderTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Reader
+
+class ReaderTests: XCTestCase {
+    func testExample() {
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}

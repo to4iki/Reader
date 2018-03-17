5841aa9fe99afa8aa6a67b945f5295bfab6b537e
Toshiki Takezawa <tsk.take815@gmail.com>
2018-03-18 02:04:04 +0900

    Add tests

--- Tests/ReaderTests/ReaderTests.swift
+++ Tests/ReaderTests/ReaderTests.swift
@@ -1,11 +1,36 @@
 import XCTest
 @testable import Reader
 
-class ReaderTests: XCTestCase {
-    func testExample() {
+final class ReaderTests: XCTestCase {
+    let addTwo: (Int) -> Int = { $0 + 2 }
+
+    func testExecute() {
+        let reader = Reader(addTwo)
+        XCTAssertEqual(reader.execute(1), 3)
+    }
+
+    func testMapOperator() {
+        let reader = Reader(addTwo)
+            .map(String.init)
+        XCTAssertEqual(reader.execute(1), "3")
+    }
+
+    func testFlatMapOperator() {
+        let reader = Reader<String, String> { $0.capitalized }
+            .flatMap { input1 in Reader<String, Cat> { input2 in Cat(name: input1 + input2) } }
+        XCTAssertEqual(reader.execute("a").name, "Aa")
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testExecute", testExecute),
+        ("testMapOperator", testMapOperator),
+        ("testFlatMapOperator", testFlatMapOperator)
     ]
+}
+
+extension ReaderTests {
+    /// Object for tests
+    private struct Cat {
+        let name: String
+    }
 }
